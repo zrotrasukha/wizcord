a7596125a8be75dc945aa613eb2ddaaa94934f79
zrotrasukha <zrotrasukha@gmail.com>
2025-07-12 21:54:59 +0530

    Add database schema definitions for users, servers, and server members

---
+++ server/src/Database/schemas/schema.ts
@@ -0,0 +1,36 @@
+import { timestamp, pgTable, varchar, text } from "drizzle-orm/pg-core";
+
+export const user = pgTable(
+    'users', {
+    id: text('id').primaryKey(),
+    username: varchar('username', { length: 225 }).notNull(),
+    email: varchar('email', { length: 225 }).notNull(),
+    avatar: text('avatar').notNull(),
+    createdAt: timestamp('createdAt').defaultNow(),
+    updatedAt: timestamp('updatedAt').defaultNow()
+}
+)
+
+export const server = pgTable(
+    'server', {
+    id: text('id').primaryKey(),
+    name: varchar('name', { length: 225 }).notNull(),
+    owner: text('owner').notNull().references(() => user.id),
+    icon: text('icon').notNull(),
+    description: text('description').notNull(),
+    createdAt: timestamp('createdAt').defaultNow(),
+    updatedAt: timestamp('updatedAt').defaultNow()
+}
+);
+
+export const serverMember = pgTable(
+    'server_member', {
+        id: text('id').primaryKey(),
+        serverId: text('serverId').notNull().references(() => server.id),
+        userId: text('userId').notNull().references(() => user.id),
+        role: varchar('role', { length: 50 }).notNull(),
+        joinedAt: timestamp('joinedAt').defaultNow(),
+        updatedAt: timestamp('updatedAt').defaultNow()
+
+    }
+)
