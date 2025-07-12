47d1f52e00b81dbe51ab86ac4f9a0c57a28534bc
zrotrasukha <zrotrasukha@gmail.com>
2025-07-12 21:54:39 +0530

    Refactor app.ts to organize middleware and route setup

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config';
 import { Hono } from "hono";
 import { clerkMiddleware } from "@hono/clerk-auth";
+import { logger } from 'hono/logger';
+import serverRouter from './src/routes/server.routes';
+import { db } from './src/Database/db';
+import { server } from './src/Database/schemas/schema';
 
 const app = new Hono();
-app.use(clerkMiddleware())
 
 
+// middlewares 
+app.use(logger());
+app.use('*', clerkMiddleware());
+app.basePath('/api/v1')
+  .route('/server', serverRouter)
 
+//version 1 
 app.get("/", (c) => {
   return c.text("Hello, world!");
 });
