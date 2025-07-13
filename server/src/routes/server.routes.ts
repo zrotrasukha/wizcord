e34c5774c7727e8e6389c48e25e07c39af5213d2
zrotrasukha <zrotrasukha@gmail.com>
2025-07-13 10:45:39 +0530

    Add server routes for retrieving all users and a specific user

---
+++ server/src/routes/server.routes.ts
@@ -0,0 +1,21 @@
+import {Hono} from 'hono';      
+import { User } from '../modals/user';
+
+const serverRouter = new Hono();
+
+serverRouter.get('/all', async (c) => {
+    const servers = await User.getAllUsers(); 
+        console.log("Servers: ", servers);
+    return c.json(servers);
+})
+
+serverRouter.get('/:id', async (c) => {
+    const id = c.req.param('id');
+    const user = await User.getUser(id);
+    if (!user) {
+        return c.json({ error: 'User not found' }, 404);
+    }
+    return c.json(user);
+})
+
+export default serverRouter;
