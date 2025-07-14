bb33cb1fb1191a48e62576c5d2fbefba958f3842
zrotrasukha <zrotrasukha@gmail.com>
2025-07-14 22:32:52 +0530

    feat: implement User class with methods to retrieve all users and a specific user from the database

---
+++ server/src/classes/user.class.ts
@@ -0,0 +1,27 @@
+import { eq } from "drizzle-orm";
+import { db } from "../Database/db";
+import { user } from "../Database/schemas/schema";
+import type { BasicUser } from "../Types/user.types";
+
+export class User {
+    constructor(
+        public id: string,
+        public username: string, 
+        public email : string,
+        public avatar : string, 
+        public createdAt?: Date,
+        public updatedAt?: Date
+    ){}
+    
+    public static async getAllUsers(){
+       const users = await db.select().from(user);
+       return users;  
+    }; 
+    
+    public static async getUser(id: string){
+        const currUser = await db.select().from(user)
+        .where(eq(user.id , id));
+        return currUser[0];
+    };
+
+}
