ea1354cbe0a6a6c07c7b677887dc6046d711f5f7
zrotrasukha <zrotrasukha@gmail.com>
2025-07-15 14:53:37 +0530

    feat: integrate API call with authorization token in index route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,7 @@
-
-import { SignedIn, useUser } from '@clerk/clerk-react'
+import { SignedIn, useAuth, useUser } from '@clerk/clerk-react'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useEffect } from 'react'
+import axios from 'axios';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -10,20 +10,45 @@ export const Route = createFileRoute('/')({
 function Index() {
   const { isLoaded, isSignedIn } = useUser();
   const navigate = useNavigate();
+ 
+  const { getToken } = useAuth()
 
+  const makeApiCall = async () => {
+   const token = await getToken()
+    
+    const response = await axios.get('http://localhost:4000/', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+    })
+    
+    console.log('Response from API:', response.data);
+    
+    return response.data
+    } 
+  
   useEffect(() => {
-    if(!isLoaded) return; 
-    if(!isSignedIn) {
+    if (!isLoaded) return;
+    if (!isSignedIn) {
       navigate({ to: '/sign-in' });
     }
   }, [isLoaded, isSignedIn]);
-  return (
-    <div>
-      <SignedIn>
-        <div className='h-screen w-screen bg-zinc-950 text-white'>
-          <div>you're signed in! 🎉</div>
-        </div>
-      </SignedIn>
-    </div>
-  );
-}
+    return (
+      <div>
+        <SignedIn>
+          <div className='h-screen w-screen bg-zinc-950 text-white'>
+            <div>you're signed in! 🎉</div>
+            <button
+              onClick={async () => {
+              await makeApiCall();
+              }}
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            >
+              Get Token
+            </button>
+           
+          </div>
+        </SignedIn>
+      </div>
+    );
+  }
