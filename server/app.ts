513678c977e9bb55f93e9fb9922a46b0a2a24f6b
zrotrasukha <zrotrasukha@gmail.com>
2025-07-15 14:53:57 +0530

    feat: enhance authentication in the main app route and add CORS middleware

--- server/app.ts
+++ server/app.ts
@@ -1,24 +1,34 @@
 import 'dotenv/config';
 import { Hono } from "hono";
-import { clerkMiddleware } from "@hono/clerk-auth";
+import { clerkMiddleware, getAuth, } from "@hono/clerk-auth";
+import { createClerkClient } from '@clerk/backend';
 import { logger } from 'hono/logger';
 import serverRouter from './src/routes/server.routes';
-import { db } from './src/Database/db';
-import { server } from './src/Database/schemas/schema';
 import { errorHandler } from '@/middlewares/error.middleware';
+import { cors } from 'hono/cors';
+import { UNAUTHORIZED } from '@/errors/statusCode';
 
 const app = new Hono();
-
-
 // middlewares 
 errorHandler(app);
 app.use(logger());
+app.use('*', cors({
+  origin: ['http://localhost:3000'], // Add your frontend URL
+  credentials: true,
+}));
 app.use('*', clerkMiddleware());
 app.basePath('/api/v1')
   .route('/server', serverRouter)
 
 //version 1 
-app.get("/", (c) => {
-  return c.text("Hello, world!");
-});
+app.get('/', (c) => {
+  const auth = getAuth(c);
+  if(!auth?.userId) {
+    return c.json({ message: 'Unauthorized' }, UNAUTHORIZED);
+  }
+  const userId = auth.userId;
+  console.log({userId});
+
+  return c.json({ message: 'Welcome to Wizcord API', userId});
+})
 export default app; 
